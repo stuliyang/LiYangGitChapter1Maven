083f156bb8b67852d6d90c0422e2cd5da9e343b5
stuliyang <1409530820@qq.com>
2020-03-11 12:00:20 +0800

    the third demo update

--- src/main/java/dao/TestDaoImpl.java
+++ src/main/java/dao/TestDaoImpl.java
@@ -7,5 +7,6 @@ public class TestDaoImpl implements TestDao {
 	@Override
 	public void sayHello() {
 		System.out.println("Hello, Study hard! 李扬！Day Day Up!");
+		System.out.println("新的一天，加油，努力！");
 	}
 }
